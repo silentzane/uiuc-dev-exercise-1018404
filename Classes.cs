e0055a04cc04d0820a90d0a4d5e138b727932d26
silentzane <vacantzane@gmail.com>
2023-11-22 11:28:48 -0600

    Adds "Person" and "Training" classes for JSON parsing.

---
+++ Classes.cs
@@ -0,0 +1,12 @@
+namespace uiuc_dev_exercise_1018404;
+
+public class Person {
+    public string Name { get; set; } = string.Empty;
+    public List<Training>? Completions { get; set; }
+}
+
+public class Training {
+    public string Name { get; set; } = string.Empty;
+    public DateOnly Timestamp { get; set; }
+    public DateOnly? Expires { get; set; }
+}
